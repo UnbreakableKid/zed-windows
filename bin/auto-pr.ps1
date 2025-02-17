7bc5f5978b86b798cf0bcf776385be7184fcba52
João Rafael Pacheco <29483407+UnbreakableKid@users.noreply.github.com>
2025-02-17 17:23:28 +0000

    add zed to bucket

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "unbreakablekid/zed-windows:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
